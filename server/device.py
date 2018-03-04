4d5b0d62eca4bd5d886419d29a407e74a81d08f1
Rohan Joshi <rohan2jos@gmail.com>
2018-03-04 01:09:29 -0600

    creating files

---
+++ server/device.py
@@ -0,0 +1,10 @@
+import logging
+
+# set up basic logging
+logging.basicConfig(filename="device.log", level=logging.DEBUG, format='%(asctime)s %(funcName)5s() %(levelname)s:  %(message)s', datefmt='%I:%M:%S %p')
+
+
+class Device:
+
+    def __init__(self):
+        logging.debug("initializing device")
